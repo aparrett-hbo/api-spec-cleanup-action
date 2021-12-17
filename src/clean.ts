c2e98b9131ef4c60718eb2cb9264ddd046224344
Anthony Parrett <anthony.parrett@wmcontractor.com>
2021-12-17 16:05:24 -0600

    Update prettier

--- src/clean.ts
+++ src/clean.ts
@@ -1,6 +1,5 @@
 import {OpenAPIObject} from 'openapi3-ts'
 
 export function clean(doc: OpenAPIObject): OpenAPIObject {
-  doc.info.version = '2'
-  return doc
+    return doc
 }
--- src/main.ts
+++ src/main.ts
@@ -5,43 +5,48 @@ import {OpenAPIObject} from 'openapi3-ts'
 import {clean} from './clean'
 
 async function run(): Promise<void> {
-  try {
-    const filePath: string = core.getInput('file')
-    const file = await fs.readFile(filePath, 'utf8')
+    try {
+        const filePath: string = core.getInput('file')
+        const file = await fs.readFile(filePath, 'utf8')
 
-    let doc: OpenAPIObject | undefined
-    const ext = filePath.split('.').pop()
-    if (ext === 'json') {
-      try {
-        doc = JSON.parse(file)
-      } catch (e) {
-        return core.setFailed(`Unable to parse spec file with error: ${e}`)
-      }
-    } else {
-      try {
-        doc = yaml.load(file) as OpenAPIObject
-      } catch (e) {
-        return core.setFailed(`Unable to parse spec file with error: ${e}`)
-      }
+        let doc: OpenAPIObject | undefined
+        const ext = filePath.split('.').pop()
+        if (ext === 'json') {
+            try {
+                doc = JSON.parse(file)
+            } catch (e) {
+                return core.setFailed(
+                    `Unable to parse spec file with error: ${e}`
+                )
+            }
+        } else {
+            try {
+                doc = yaml.load(file) as OpenAPIObject
+            } catch (e) {
+                return core.setFailed(
+                    `Unable to parse spec file with error: ${e}`
+                )
+            }
+        }
+
+        if (!doc) {
+            return core.setFailed('Unable to parse spec file.')
+        }
+
+        const cleanDoc = clean(doc)
+
+        if (ext !== 'json') {
+            await fs.writeFile(filePath, yaml.dump(cleanDoc))
+        } else {
+            await fs.writeFile(filePath, JSON.stringify(cleanDoc, null, 4))
+        }
+    } catch (error) {
+        if (error instanceof Error) return core.setFailed(error.message)
+        if (error instanceof String) return core.setFailed(error as string)
+        core.setFailed(
+            `Unable to perform cleanup due to unknown error: ${error}`
+        )
     }
-
-    if (!doc) {
-      return core.setFailed('Unable to parse spec file.')
-    }
-
-    const cleanedDoc = clean(doc)
-
-    if (ext !== 'json') {
-      core.warning(ext as string)
-      await fs.writeFile(filePath, yaml.dump(cleanedDoc))
-    } else {
-      await fs.writeFile(filePath, JSON.stringify(cleanedDoc, null, 4))
-    }
-  } catch (error) {
-    if (error instanceof Error) return core.setFailed(error.message)
-    if (error instanceof String) return core.setFailed(error as string)
-    core.setFailed(`Unable to perform cleanup due to unknown error: ${error}`)
-  }
 }
 
 run()
